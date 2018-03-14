a318562fdb95addc8979de85386898b799a28bd9
LAPTOP-CVJ76E6R\Zac <isaacjiajhe@gmail.com>
2018-03-14 11:09:29 +0800

    create project

---
+++ MediaConvertor/MediaConvertor.h
@@ -0,0 +1,31 @@
+// MediaConvertor.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMediaConvertorApp:
+// See MediaConvertor.cpp for the implementation of this class
+//
+
+class CMediaConvertorApp : public CWinApp
+{
+public:
+	CMediaConvertorApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMediaConvertorApp theApp;
